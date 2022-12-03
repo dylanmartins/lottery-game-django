ec2150713049ea63e626f8c1caf5db7789f3ea27
dylan.andrade <dylan.andrade@sendcloud.com>
2022-12-03 13:45:35 +0100

    chore: adds more unit tests and sort for numbers

--- src/lotteries/serializers.py
+++ src/lotteries/serializers.py
@@ -1,9 +1,19 @@
 from lotteries.models import LotteryGame
+from users.models import User
 from rest_framework import serializers
 
 
 class LotteryGameSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(required=True, queryset=User.objects.all())
+    numbers = serializers.ListField(required=True, child=serializers.IntegerField())
 
     class Meta:
         model = LotteryGame
         fields = "__all__"
+        read_only_fields = ["uuid", "user", "winning_game", "created_at"]
+
+    def validate_numbers(self, numbers):
+        # We sort the numbers here to create a pattern,
+        # this way it will be easier to filter by numbers in the future
+        numbers.sort()
+        return numbers
--- src/lotteries/tests/test_views.py
+++ src/lotteries/tests/test_views.py
@@ -34,7 +34,11 @@ def test_get_lottery_games_api__success(auth_api_client, valid_user, valid_lotte
     response = auth_api_client.get(reverse("lotteries:lottery-list"))
     assert response.status_code == 200
 
-    assert len(LotteryGame.objects.filter(user=valid_user)) == 1
+    lottery_game = response.data[0]
+    assert lottery_game["uuid"] == str(valid_lottery_game.pk)
+    assert lottery_game["numbers"] == list(range(LOTTERY_GAME_SIZE))
+    assert lottery_game["winning_game"] == False
+    assert str(lottery_game["user"]) == str(valid_user.pk)
 
 
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
@@ -48,3 +52,43 @@ def test_get_detail_lottery_games_api__success(auth_api_client, valid_user, vali
     assert response.data["numbers"] == list(range(LOTTERY_GAME_SIZE))
     assert response.data["winning_game"] == False
     assert str(response.data["user"]) == str(valid_user.pk)
+
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_create_lottery_game_api__numbers_are_sorted_when_creating(auth_api_client, valid_create_lottery_payload, valid_user):
+
+    assert len(LotteryGame.objects.filter(user=valid_user)) == 0
+
+    valid_create_lottery_payload["numbers"] = [66, 12, 1, 102]
+    response = auth_api_client.post(reverse("lotteries:lottery-list"), data=valid_create_lottery_payload)
+    assert response.status_code == 201
+    assert response.data["numbers"] == [1, 12, 66, 102]
+
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_create_lottery_game_api__setting_winning_game_as_true(auth_api_client, valid_create_lottery_payload, valid_user):
+
+    assert len(LotteryGame.objects.filter(user=valid_user)) == 0
+
+    valid_create_lottery_payload["winning_game"] = True
+    response = auth_api_client.post(reverse("lotteries:lottery-list"), data=valid_create_lottery_payload)
+    assert response.status_code == 201
+    # it's still False because this field is read only
+    assert response.data["winning_game"] == False
+
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+def test_get_lottery_games_api__check_winning_game(auth_api_client, valid_user, valid_lottery_game):
+
+    valid_lottery_game.winning_game = True
+    valid_lottery_game.save()
+
+    response = auth_api_client.get(reverse("lotteries:lottery-list"))
+    assert response.status_code == 200
+
+    lottery_game = response.data[0]
+    assert lottery_game["uuid"] == str(valid_lottery_game.pk)
+    assert lottery_game["numbers"] == list(range(LOTTERY_GAME_SIZE))
+    # Set as True
+    assert lottery_game["winning_game"] == True
+    assert str(lottery_game["user"]) == str(valid_user.pk)
